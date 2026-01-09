dbe97250842ce6e1d573df717361d1f156de1636
minhkhoiv12 <khoitk14033@gmail.com>
2026-01-09 16:38:57 +0700

    implement real-time messaging, presence status(online/offline) & profile updates

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -4,7 +4,7 @@ import { ENV } from "./env.js";
 export const generateToken = (userId, res) => {
   const { JWT_SECRET } = ENV;
   if (!JWT_SECRET) {
-    throw new Error("Chưa cấu hình JWT_SECRET");
+    throw new Error("JWT_SECRET is not configured");
   }
 
   const token = jwt.sign({ userId }, JWT_SECRET, {
@@ -14,7 +14,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // MS
     httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
+    sameSite: "none", // CSRF attacks
     secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
+import { io } from "socket.io-client";
 
-
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://chapapp-nbi9.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -67,5 +67,36 @@ export const useAuthStore = create((set, get) => ({
       toast.error("Lỗi khi đăng xuất");
       console.log("Lỗi khi đăng xuất:", error);
     }
+  },
+  updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Cập nhật profile thành công");
+    } catch (error) {
+      console.log("Có lỗi cập nhật profile:", error);
+      toast.error(error.response.data.message);
+    }
+  },
+  connectSocket: () => {
+    const { authUser } = get();
+    if (!authUser || get().socket?.connected) return;
+
+    const socket = io(BASE_URL, {
+      withCredentials: true, // this ensures cookies are sent with the connection
+    });
+
+    socket.connect();
+
+    set({ socket });
+
+    // listen for online users event
+    socket.on("getOnlineUsers", (userIds) => {
+      set({ onlineUsers: userIds });
+    });
+  },
+
+  disconnectSocket: () => {
+    if (get().socket?.connected) get().socket.disconnect();
   },
 }));
